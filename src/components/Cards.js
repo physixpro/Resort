2eaf8005ec08e47e490eedd4957b7c48da3ba473
Keagan St. Rose <keaganstrose@gmail.com>
2021-06-16 20:06:26 -0700

    added showBlurStateTwo

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -8,8 +8,8 @@ import Typography from "@material-ui/core/Typography";
 import "./cards.css";
 
 const SimpleCard = () => {
-
-  const [showBlurOne,setShowBlurOne] = useState("")
+  const [showBlurOne, setShowBlurOne] = useState("");
+  const [showBlurTwo, setShowBlurTwo] = useState("");
   const useStyles = makeStyles({
     root: {
       backgroundColor: "orange",
@@ -35,7 +35,6 @@ const SimpleCard = () => {
 
   return (
     <div className="card-container">
-
       <Card className={classes.root} variant="outlined">
         <img
           src="https://sharp-wescoff-20e282.netlify.app/static/media/aOne.0b6d81c7.jpg"
@@ -44,27 +43,25 @@ const SimpleCard = () => {
           height="400"
         />
       </Card>
-     
-        <Card className={classes.root} variant="outlined">
-          <img
-            src="https://sharp-wescoff-20e282.netlify.app/static/media/aTwo.4fad255a.jpg"
-            alt=""
-            width="525"
-            height="400"
-          />
-        </Card>
-    
-        <Card className={classes.root} variant="outlined">
-          <img
-            src="https://sharp-wescoff-20e282.netlify.app/static/media/aThree.005f5082.jpg"
-            alt=""
-            width="525"
-            height="400"
-          />
-        </Card>
-    
+
+      <Card className={classes.root} variant="outlined">
+        <img
+          src="https://sharp-wescoff-20e282.netlify.app/static/media/aTwo.4fad255a.jpg"
+          alt=""
+          width="525"
+          height="400"
+        />
+      </Card>
+
+      <Card className={classes.root} variant="outlined">
+        <img
+          src="https://sharp-wescoff-20e282.netlify.app/static/media/aThree.005f5082.jpg"
+          alt=""
+          width="525"
+          height="400"
+        />
+      </Card>
     </div>
-   
   );
 };
 
@@ -87,7 +84,7 @@ const Cards = () => {
   });
 
   return (
-    <div >
+    <div>
       <SimpleCard />
     </div>
   );
