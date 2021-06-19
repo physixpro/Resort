14d5e7f8c3604218cdaed782d5f5cfc2c9f0319d
Keagan St. Rose <keaganstrose@gmail.com>
2021-06-19 11:03:15 -0700

    added new image with logo

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import resort from '../videos/resort.mp4';
-import resortOne from "../images/resortOne.jpeg";
-import resortTwo from "../images/resortTwo.jpeg";
+import resortOne from "../images/resortOne.jpg";
+
 import './video.css'
 
 const Video = () => {
     return (
         <div className="video-container">
 {/* <video loop autoPlay src={resort} type="video/mp4" /> */}
-<img src={resortOne} alt="" /> <h1> Isla Escondida</h1>
-
-
+<img src={resortOne} alt="" />
         </div>
         
     )
