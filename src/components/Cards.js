829dfb434a3bdbc7c1739a628377f81ce177961b
Keagan St. Rose <keaganstrose@gmail.com>
2021-06-18 21:25:27 -0700

    solved mouse out issue by moving animate effect to img and took it off of the card class itself

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -12,20 +12,16 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import "./cards.css";
 
-
 const SimpleCard = () => {
-  
-
   useEffect(() => {
     AOS.init();
     AOS.refresh();
   }, []);
-  
+
   const [showCardOne, setShowCardOne] = useState(true);
   const [showCardTwo, setShowCardTwo] = useState(true);
   const [showCardThree, setShowCardThree] = useState(true);
 
- 
   const useStyles = makeStyles({
     root: {
       backgroundColor: "none",
@@ -53,14 +49,15 @@ const SimpleCard = () => {
     <div className="card-container">
       {/***************** First Card ******************/}
       {showCardOne ? (
-        <Card data-aos="fade-up"
-        data-aos-duration="1000"
+        <Card
           onMouseOver={() => setShowCardOne(false)}
-         
           setclassName={classes.root}
           variant="outlined"
         >
-          <img  id="first-image-clear"
+          <img
+            data-aos="fade-up"
+            data-aos-duration="2000"
+            id="first-image-clear"
             src="https://sharp-wescoff-20e282.netlify.app/static/media/aOne.0b6d81c7.jpg"
             alt=""
             width="525"
@@ -68,30 +65,70 @@ const SimpleCard = () => {
           />
         </Card>
       ) : (
-        <img className="blurredImages" id="firstImage" onMouseOut={() => setShowCardOne(true)} className="blurredImageOne" src={imageOneBlurred} width="525" height="400" alt="blurOne" />
-      )}
- {/***************** Second Card ******************/}
-{showCardTwo ? (
-      <Card data-aos-duration="3000" data-aos="fade-up" onMouseOver={() => setShowCardTwo(false)} className={classes.root} variant="outlined">
         <img
-          src="https://sharp-wescoff-20e282.netlify.app/static/media/aTwo.4fad255a.jpg"
-          alt=""
+          className="blurredImages"
+          id="firstImage"
+          onMouseOut={() => setShowCardOne(true)}
+          className="blurredImageOne"
+          src={imageOneBlurred}
           width="525"
           height="400"
+          alt="blurOne"
         />
-      </Card> ) : ( <img  className="blurredImages" onMouseOut={() => setShowCardTwo(true)}  src={imageTwoBlurred} width="525" height="400" alt="blurTwo"  /> )}
+      )}
+      {/***************** Second Card ******************/}
+      {showCardTwo ? (
+        <Card
+          onMouseOver={() => setShowCardTwo(false)}
+          className={classes.root}
+          variant="outlined"
+        >
+          <img
+            data-aos="fade-up"
+            data-aos-duration="2000"
+            src="https://sharp-wescoff-20e282.netlify.app/static/media/aTwo.4fad255a.jpg"
+            alt=""
+            width="525"
+            height="400"
+          />
+        </Card>
+      ) : (
+        <img
+          className="blurredImages"
+          onMouseOut={() => setShowCardTwo(true)}
+          src={imageTwoBlurred}
+          width="525"
+          height="400"
+          alt="blurTwo"
+        />
+      )}
 
- {/***************** Third Card ******************/}
- {showCardThree ? (
-      <Card data-aos-duration="1000" data-aos="fade-up" onMouseOver={ () => setShowCardThree(false)} className={classes.root} variant="outlined">
-        <img 
-          src="https://sharp-wescoff-20e282.netlify.app/static/media/aThree.005f5082.jpg"
-          alt=""
+      {/***************** Third Card ******************/}
+      {showCardThree ? (
+        <Card
+          onMouseOver={() => setShowCardThree(false)}
+          className={classes.root}
+          variant="outlined"
+        >
+          <img
+            data-aos="fade-up"
+            data-aos-duration="2000"
+            src="https://sharp-wescoff-20e282.netlify.app/static/media/aThree.005f5082.jpg"
+            alt=""
+            width="525"
+            height="400"
+          />
+        </Card>
+      ) : (
+        <img
+          className="blurredImages"
+          onMouseOut={() => setShowCardThree(true)}
+          src={imageThreeBlurred}
           width="525"
           height="400"
+          alt="blurThree"
         />
-      </Card>
- ) : ( <img  className="blurredImages" onMouseOut={ () => setShowCardThree(true)}src={imageThreeBlurred}  width="525" height="400" alt="blurThree" />)}
+      )}
     </div>
   );
 };
