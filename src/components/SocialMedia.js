59d348f1ce84e55345e939d7535efc005f80bf3d
Keagan St. Rose <keaganstrose@gmail.com>
2021-06-19 09:30:08 -0700

    added animation to social headers

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -3,7 +3,8 @@ import "./socialmedia.css";
 
 const SocialMedia = () => {
     return (
-        <div>
+        <div   data-aos="fade-up"
+        data-aos-duration="2000">
             <h3 className="headers" id="social-hub">SOCIAL HUB</h3>
             &nbsp;
             <h1 className="headers" id="social-join">JOIN THE CONVERSATION</h1>
